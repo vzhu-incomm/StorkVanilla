31d8ce4eac6154f2273716f0e882a4e8cab4e30e
Vincent Zhu <vzhu@incomm.com>
2022-02-24 13:34:24 -0500

    storks reappear (jank but its w/e)

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 function loadAnimation() {
 
     timer();
@@ -13,14 +12,75 @@ function timer() {
 }
 
 function disp() {
-    var x = document.getElementById('stork1').style.left;
-    xnum = x.slice(0, -2);
-    xnum = parseFloat(xnum);
-    document.getElementById('stork1').style.left = xnum + 0.05 + "vw";
 
-    var y = document.getElementById('stork1').style.top;
-    ynum = y.slice(0, -2);
-    ynum = parseFloat(ynum);
-    document.getElementById('stork1').style.top = ynum - 0.05 + "vh";
+    for (let i = 1; i <= 10; i++) {
+        storknum = 'stork' + i;
+        var x = document.getElementById(storknum).style.left;
+        xnum = x.slice(0, -2);
+        xnum = parseFloat(xnum);
+        document.getElementById(storknum).style.left = xnum + 0.025 + "vw";
+
+        var y = document.getElementById(storknum).style.top;
+        ynum = y.slice(0, -2);
+        ynum = parseFloat(ynum);
+        document.getElementById(storknum).style.top = ynum - 0.025 + "vh";
+
+
+        rect = document.getElementById(storknum).getBoundingClientRect();
+        box = document.getElementById('border').getBoundingClientRect();
+
+        if (rect.x > box.right || rect.bottom < box.y) {
+
+            switch (i) {
+                case 1:
+                    // console.log(document.getElementById(storknum).style.left);
+                    // console.log(document.getElementById(storknum).style.top);
+                    document.getElementById(storknum).style.left = "-15vw";
+                    document.getElementById(storknum).style.top = "62vh";
+                    break;
+                case 2:
+                    // console.log(document.getElementById(storknum).style.left);
+                    // console.log(document.getElementById(storknum).style.top);
+                    document.getElementById(storknum).style.left = "-20vw";
+                    document.getElementById(storknum).style.top = "72vh";
+                    break;
+                case 3:
+                    document.getElementById(storknum).style.left = "-5vw";
+                    document.getElementById(storknum).style.top = "62vh";
+                    break;
+                case 4:
+                    document.getElementById(storknum).style.left = "-27vw";
+                    document.getElementById(storknum).style.top = "32vh";
+                    break;
+                case 5:
+                    document.getElementById(storknum).style.left = "-35vw";
+                    document.getElementById(storknum).style.top = "40vh";
+                    break;
+                case 6:
+                    document.getElementById(storknum).style.left = "-40vw";
+                    document.getElementById(storknum).style.top = "50vh";
+                    break;
+                // case 7:
+                //     document.getElementById(storknum).style.left = "15vw";
+                //     document.getElementById(storknum).style.top = "35vh";
+                //     break;
+                case 8:
+                    document.getElementById(storknum).style.left = "-35vw";
+                    document.getElementById(storknum).style.top = "-10vh";
+                    break;
+                case 9:
+                    document.getElementById(storknum).style.left = "-55vw";
+                    document.getElementById(storknum).style.top = "15vh";
+                    break;
+                case 10:
+                    document.getElementById(storknum).style.left = "-18vw";
+                    document.getElementById(storknum).style.top = "21vh";
+                    break;
+
+            }
+
+        }
+
+    }
 
 }
